e5c5da889a1c5bb87ce58c52dd3043cbd0186e8b
tensorchief <silvia.b.reynolds@gmail.com>
2020-12-28 10:13:01 +0100

    scan to outdir

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import subprocess
+import os
+import shutil
 
-def main(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main(directory='.scan/'):
+
+    # Create out dir
+    if os.path.exists(directory):
+        shutil.rmtree(directory)
+    os.mkdir(directory)
+
+    # compile scan command
     command = ['scanimage',
                '--format', 'tiff',
                '--resolution', '300',
@@ -11,13 +18,15 @@ def main(name):
                '--page-height', '297',
                '--ald=yes',
                '--device-name', 'fujitsu:ScanSnap iX500:1201996',
-               '--batch=./%02d.tiff']
+               f'--batch={directory}%02d.tiff']
+
+    # scan
     subprocess.check_output(command,
                             stderr=subprocess.STDOUT)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    main('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
