6343d15567a3ac16d6b1c78fa589e10e7a360d5d
tensorchief <silvia.b.reynolds@gmail.com>
2020-12-29 13:30:32 +0100

    initiate Vue madness

---
+++ src/components/ScanButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <a target="_blank" v-bind:href="scan">{{ title }}</a>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'ScanButton',
+  props: ['title'],
+  computed: {
+    scan () {
+      return `${axios.defaults.baseURL}api/v1.0/scan`
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
